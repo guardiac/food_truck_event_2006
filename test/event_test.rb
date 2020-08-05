00a60126625b95d8bb8e654249ce22ae1925d369
Angela Guardia <angelaguardia20@gmail.com>
2020-08-05 08:28:22 -0700

    Add add_food_truck method to Event

--- test/event_test.rb
+++ test/event_test.rb
@@ -17,4 +17,17 @@ class EventTest < Minitest::Test
     assert_equal "South Pearl Street Farmers Market", event.name
     assert_equal [], event.food_trucks
   end
+
+  def test_test_it_can_add_food_truck
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    assert_equal [food_truck1, food_truck2, food_truck3], event.food_trucks
+  end
 end
