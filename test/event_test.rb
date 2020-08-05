d636af0ad6e858232cc0b576a246bde61952ce4e
Angela Guardia <angelaguardia20@gmail.com>
2020-08-05 09:19:45 -0700

    Add date method to Event

--- test/event_test.rb
+++ test/event_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/event'
 require './lib/food_truck'
 require './lib/item'
@@ -222,5 +223,42 @@ class EventTest < Minitest::Test
     expected = ["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"]
 
     assert_equal expected, event.sorted_item_list
+  end
+
+  def test_it_knows_the_date
+    event = Event.new("South Pearl Street Farmers Market")
+    event.stubs(:date).returns("24/02/2020")
+
+    assert_equal "24/02/2020", event.date
+  end
+
+  def test_it_can_sell
+    skip
+    event = Event.new("South Pearl Street Farmers Market")
+
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    item5 = Item.new({name: 'Onion Pie', price: '$25.00'})
+
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+
   end
 end
