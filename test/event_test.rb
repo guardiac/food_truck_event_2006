023a0481b609345deb89679dd23f4628afead3d9
Angela Guardia <angelaguardia20@gmail.com>
2020-08-05 08:31:23 -0700

    Add food_truck_names method to Event

--- test/event_test.rb
+++ test/event_test.rb
@@ -30,4 +30,19 @@ class EventTest < Minitest::Test
 
     assert_equal [food_truck1, food_truck2, food_truck3], event.food_trucks
   end
+
+  def test_it_knows_food_truck_names
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    expected = ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+
+    assert_equal expected, event.food_truck_names
+  end
 end
