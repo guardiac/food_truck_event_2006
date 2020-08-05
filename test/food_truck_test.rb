366d52fc7c01828add3a75046f9a1c54ff08901c
Angela Guardia <angelaguardia20@gmail.com>
2020-08-05 08:17:40 -0700

    Add stock and check_stock method to FoodTruck

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -17,4 +17,26 @@ class FoodTruckTest < Minitest::Test
     assert_equal expected , food_truck.inventory
   end
 
+  def test_it_can_stock_and_check_stock
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    assert_equal 0, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 30)
+    expected = {item1 => 30}
+
+    assert_equal expected, food_truck.inventory
+
+    food_truck.stock(item1, 25)
+    expected = {item1 => 55}
+
+    assert_equal expected, food_truck.inventory
+
+    food_truck.stock(item2, 12)
+    expected = {item1 => 55, item2 => 12}
+
+    assert_equal expected, food_truck.inventory
+  end
 end
